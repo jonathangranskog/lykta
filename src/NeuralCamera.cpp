87a27d611b403d92a1aa1fca3cf3d22b52ef18a2
Jonathan Granskog <jonathan.granskog@aalto.fi>
2018-11-27 16:16:25 +0100

    FIRST NEURAL CAMERA IMAGE

--- src/NeuralCamera.cpp
+++ src/NeuralCamera.cpp
@@ -7,21 +7,20 @@
 using namespace Lykta;
 
 NeuralCamera::NeuralCamera(const std::string& modelFile, const std::string& dataFile, glm::mat4 camToWorld, glm::ivec2 res) {
-    module = torch::jit::load(modelFile);
+    module = torch::jit::load("/Users/jonathan/Documents/lykta/scenes/spheres/petzval.pt");
     resolution = res;
     aspect = resolution.x / (float)resolution.y;
     sensorSize = glm::vec2(0.024f, 0.024f * 1.f/aspect);
-    cameraToWorld = lookAt(glm::vec3(0, 0, 2), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+    cameraToWorld = camToWorld;
     
-
-    std::ofstream file;
-    file.open(dataFile);
+    std::ifstream file;
+    file.open("/Users/jonathan/Documents/lykta/scenes/spheres/petzval.txt");
     if (file.is_open()) {
         std::string meanLine, stdLine, frontZLine, rearRadiusLine;
-        assert(std::getline(file, meanLine));
-        assert(std::getline(file, stdLine));
-        assert(std::getline(file, frontZLine));
-        assert(std::getline(file, rearRadiusLine));
+        std::getline(file, meanLine);
+        std::getline(file, stdLine);
+        std::getline(file, frontZLine);
+        std::getline(file, rearRadiusLine);
         std::stringstream meanss(meanLine);
         std::string token;
         while (std::getline(meanss, token, ' ')) {
@@ -34,11 +33,6 @@ NeuralCamera::NeuralCamera(const std::string& modelFile, const std::string& data
         frontZ = std::stof(frontZLine);
         rearRadius = std::stof(rearRadiusLine);
     }
-
-    //std::cout << frontZ << " " << rearRadius << std::endl;
-    //for (int i = 0; i < means.size(); i++) {
-    //    std::cout << means[i] << " " << stds[i] << std::endl;
-    //}
 }
 
 // TODO: Better way of doing this........
@@ -51,24 +45,26 @@ void NeuralCamera::normalizeInput(glm::vec2& orig, glm::vec3& dir) const {
 }
 
 void NeuralCamera::denormalizeOutput(float& success, glm::vec3& orig, glm::vec3& dir) const {
-    success = (success - means[5]) / stds[5];
-    orig.x = (orig.x - means[6]) / stds[6];
-    orig.y = (orig.y - means[7]) / stds[7];
-    orig.z = (orig.z - means[8]) / stds[8];
-    dir.x = (dir.x - means[9]) / stds[9];
-    dir.y = (dir.y - means[10]) / stds[10];
-    dir.z = (dir.z - means[11]) / stds[11];
+    success = success * stds[5] + means[5];
+    orig.x = orig.x * stds[6] + means[6];
+    orig.y = orig.y * stds[7] + means[7];
+    orig.z = orig.z * stds[8] + means[8];
+    dir.x = dir.x * stds[9] + means[9];
+    dir.y = dir.y * stds[10] + means[10];
+    dir.z = dir.z * stds[11] + means[11];
 }
 
 glm::vec3 NeuralCamera::createRay(Ray& ray, const glm::vec2& pixel, const glm::vec2& sample) const {
+    
     if (module) {
         std::vector<torch::jit::IValue> inputs;
         // Create ray and create input
-        glm::vec2 np = glm::vec2(pixel.x / resolution.x, pixel.y / resolution.y);
+        glm::vec2 np = glm::vec2(pixel.x / resolution.x, 1.f - pixel.y / resolution.y);
         glm::vec2 pFilm = np * sensorSize - sensorSize / 2.f;
         glm::vec3 pRear = glm::vec3(-rearRadius + sample.x * 2 * rearRadius, -rearRadius + sample.y * 2 * rearRadius, frontZ);
         glm::vec3 direction = pRear - glm::vec3(pFilm, 0.f);
         direction = glm::normalize(direction);
+        normalizeInput(pFilm, direction);
 
         // Create input tensor
         torch::Tensor input = torch::ones({5});
@@ -83,12 +79,18 @@ glm::vec3 NeuralCamera::createRay(Ray& ray, const glm::vec2& pixel, const glm::v
         at::Tensor output = module->forward(inputs).toTensor();
         output = output.to(at::kFloat);
         float* data = output.data<float>();
-
+        
         // Extract output [success, orig.x, orig.y, orig.z, dir.x, dir.y, dir.z]
-        bool success = data[0] > 0.5f;
-        if (success) {
-            glm::vec3 pos = glm::vec3(data[1], data[2], data[3]);
-            glm::vec3 dir = glm::vec3(data[4], data[5], data[6]);
+        float success = data[0];
+        glm::vec3 pos = glm::vec3(data[1], data[2], data[3]);
+        glm::vec3 dir = glm::vec3(data[4], data[5], data[6]);
+
+        denormalizeOutput(success, pos, dir);
+
+        bool passed = success > 0.5f;
+
+        if (passed) {
+            dir = glm::normalize(dir);
             ray.o = glm::vec3(cameraToWorld * glm::vec4(pos, 1));
             ray.d = glm::vec3(cameraToWorld * glm::vec4(dir, 0));
             ray.t = glm::vec2(EPS, INFINITY);
